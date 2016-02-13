196806dc329e6868c57fda347994bef6e2cfab13
Arne Beer <arne@twobeer.de>
2016-02-13 22:32:52 +0100

    Readme

--- commands/client_parser.py
+++ commands/client_parser.py
@@ -1,7 +1,9 @@
 import argparse
 import getpass
 
-client_parser = argparse.ArgumentParser(description='Parser for arrrsync client.')
+client_parser = argparse.ArgumentParser(
+    description='The arrrsync client. Arrrsync will try to get all config data from your ssh config file. \n If this doesn\'t work you need to pass them as arguments.'
+)
 
 client_parser.add_argument('host', type=str, help='The address of the host.')
 client_parser.add_argument('--user', type=str, default=getpass.getuser(), help='The user you want to connect to.')
